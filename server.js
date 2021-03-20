7f8c4084cce337680352001e26da24d507d0fc03
Matthew Heering <matthew.c.heering01@gmail.com>
2021-03-20 18:14:06 -0500

    adding chart js

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const morgan = require("morgan");
 const mongoose = require("mongoose");
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+app.use(morgan("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
