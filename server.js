4b6e25c1aa791e830b51fcbf5835d59ea4834165
Matthew Heering <matthew.c.heering01@gmail.com>
2021-03-20 17:40:57 -0500

    adding environment variable to deploy application to heroku

--- server.js
+++ server.js
@@ -11,11 +11,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
-mongoose.connect(MONGODB_URI, {
-      useNewUrlParser: true,
-      useFindAndModify: false
-})
+mongoose.connect(
+      process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+      {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+            useFindAndModify: false
+      }
+);
+
 
 require("./routes/api-routes")(app);
 require("./routes/html-routes")(app);
