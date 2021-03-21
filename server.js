e4738265c20aa9ef304e8ccb80d21f24ed667bee
Matthew Heering <matthew.c.heering01@gmail.com>
2021-03-20 19:52:05 -0500

    fixed db

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+require("dotenv").config()
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -9,14 +10,28 @@ app.use(express.json());
 app.use(express.static('public'));
 
 mongoose.connect(
-      process.env.MONGODB_URI || 'mongodb://localhost/workout',
+      process.env.MONGODB_URI,
       {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useCreateIndex: true,
-            useFindAndModify: false
+            useFindAndModify: true
       }
 );
+
+mongoose.connection.on('connected', function () {
+      console.log('Mongoose default connection open');
+});
+
+// If the connection throws an error
+mongoose.connection.on('error', function (err) {
+      console.log('Mongoose default connection error: ' + err);
+});
+
+// When the connection is disconnected
+mongoose.connection.on('disconnected', function () {
+      console.log('Mongoose default connection disconnected');
+});
 
 
 require("./routes/api-routes")(app);
